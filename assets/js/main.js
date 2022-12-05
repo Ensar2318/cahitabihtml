4531daed5272e8cd5d61ae04d542c9efc97408ff
Ensar2318 <68289958+Ensar2318@users.noreply.github.com>
2022-12-05 19:25:33 +0300

    qwewq

--- assets/js/main.js
+++ assets/js/main.js
@@ -263,6 +263,161 @@ $(document).ready(function () {
 
 
 });
+
+// kredit cart filter
+
+const cardnumber = document.getElementById('creditNumber');
+const expirationdate = document.getElementById('creditDate');
+const securitycode = document.getElementById('creditSecurity');
+const creditname = document.getElementById('creditName');
+
+
+
+var cardnumber_mask = new IMask(cardnumber, {
+    mask: [
+        {
+            mask: '0000 000000 00000',
+            regex: '^3[47]\\d{0,13}',
+            cardtype: 'american express'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^(?:6011|65\\d{0,2}|64[4-9]\\d?)\\d{0,12}',
+            cardtype: 'discover'
+        },
+        {
+            mask: '0000 000000 0000',
+            regex: '^3(?:0([0-5]|9)|[689]\\d?)\\d{0,11}',
+            cardtype: 'diners'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^(5[1-5]\\d{0,2}|22[2-9]\\d{0,1}|2[3-7]\\d{0,2})\\d{0,12}',
+            cardtype: 'mastercard'
+        },
+        // {
+        //     mask: '0000-0000-0000-0000',
+        //     regex: '^(5019|4175|4571)\\d{0,12}',
+        //     cardtype: 'dankort'
+        // },
+        // {
+        //     mask: '0000-0000-0000-0000',
+        //     regex: '^63[7-9]\\d{0,13}',
+        //     cardtype: 'instapayment'
+        // },
+        {
+            mask: '0000 000000 00000',
+            regex: '^(?:2131|1800)\\d{0,11}',
+            cardtype: 'jcb15'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^(?:35\\d{0,2})\\d{0,12}',
+            cardtype: 'jcb'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^(?:5[0678]\\d{0,2}|6304|67\\d{0,2})\\d{0,12}',
+            cardtype: 'maestro'
+        },
+        // {
+        //     mask: '0000-0000-0000-0000',
+        //     regex: '^220[0-4]\\d{0,12}',
+        //     cardtype: 'mir'
+        // },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^4\\d{0,15}',
+            cardtype: 'visa'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: '^62\\d{0,14}',
+            cardtype: 'unionpay'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            cardtype: 'Unknown'
+        }
+    ],
+    dispatch: function (appended, dynamicMasked) {
+        var number = (dynamicMasked.value + appended).replace(/\D/g, '');
+
+        for (var i = 0; i < dynamicMasked.compiledMasks.length; i++) {
+            let re = new RegExp(dynamicMasked.compiledMasks[i].regex);
+            if (number.match(re) != null) {
+                return dynamicMasked.compiledMasks[i];
+            }
+        }
+    }
+});
+
+//Mask the Expiration Date
+var expirationdate_mask = new IMask(expirationdate, {
+    mask: 'MM{/}YY',
+    groups: {
+        YY: new IMask.MaskedPattern.Group.Range([0, 99]),
+        MM: new IMask.MaskedPattern.Group.Range([1, 12]),
+    }
+});
+
+//Mask the security code
+var securitycode_mask = new IMask(securitycode, {
+    mask: '000',
+});
+
+
+creditname.addEventListener('input', function () {
+    if (creditname.value.length == 0) {
+        document.getElementById('creditNameArea').innerHTML = 'Ceyda Kasabalı';
+        document.getElementById('backnamecredit').innerHTML = 'Ceyda Kasabalı';
+    } else {
+        document.getElementById('creditNameArea').innerHTML = this.value;
+        document.getElementById('backnamecredit').innerHTML = this.value;
+    }
+});
+
+cardnumber_mask.on('accept', function () {
+    if (cardnumber_mask.value.length == 0) {
+        document.getElementById('creditNumberArea').innerHTML = '**** **** **** 1901';
+    } else {
+        document.getElementById('creditNumberArea').innerHTML = cardnumber_mask.value;
+    }
+});
+
+
+expirationdate_mask.on('accept', function () {
+    if (expirationdate_mask.value.length == 0) {
+        document.getElementById('creditDateArea').innerHTML = '04/28';
+    } else {
+        document.getElementById('creditDateArea').innerHTML = expirationdate_mask.value;
+    }
+});
+
+securitycode_mask.on('accept', function () {
+    if (securitycode_mask.value.length == 0) {
+        document.getElementById('securityCode').innerHTML = '985';
+    } else {
+        document.getElementById('securityCode').innerHTML = securitycode_mask.value;
+    }
+});
+
+creditname.addEventListener('focus', function () {
+    document.querySelector('.ui-credit-v1').classList.remove('flipped');
+});
+
+cardnumber.addEventListener('focus', function () {
+    document.querySelector('.ui-credit-v1').classList.remove('flipped');
+});
+
+expirationdate.addEventListener('focus', function () {
+    document.querySelector('.ui-credit-v1').classList.remove('flipped');
+});
+
+securitycode.addEventListener('focus', function () {
+    document.querySelector('.ui-credit-v1').classList.add('flipped');
+});
+
 // Filter js init işlemi
 // $(document).ready(function () {
 //     var DocumentOptions = {
